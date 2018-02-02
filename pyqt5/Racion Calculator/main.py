9b446072d68a2235bfaf5101c79de83e1f61b763
vh <vh@localhost.localdomain>
2018-02-03 02:08:16 +0300

    Ver 2.0 " Global update, Sqlite, bug fix, optomizacion"

--- pyqt5/Racion Calculator/main.py
+++ pyqt5/Racion Calculator/main.py
@@ -6,6 +6,9 @@ from PyQt5.QtWidgets import *
 from PyQt5 import QtCore
 from res.ui import Ui_Dialog
 import re
+import sqlite3
+conn = sqlite3.connect('./data/data.db')
+c = conn.cursor()
 
 class MyApp(QMainWindow):
     def __init__(self):
@@ -15,7 +18,6 @@ class MyApp(QMainWindow):
 
         self.ui.pushButton.clicked.connect(self.calc)
         self.ui.pushButton_4.clicked.connect(qApp.quit)
-        
 
     def calc(self):
         _translate = QtCore.QCoreApplication.translate
@@ -77,168 +79,153 @@ class MyApp(QMainWindow):
         self.open_bd(phen, yach, kuk, m_k, fish, prem_8184, gmih, shr, mk2, yam, trm, msrs, r_k, prem_8203)
 
     def open_bd(self, phen, yach, kuk, m_k, fish, prem_8184, gmih, shr, mk2, yam, trm, msrs, r_k, prem_8203):
-        fille = open("./data/phen.ini", "r")
-        kdg_phen = float(''.join(re.findall(r'\d+', fille.readline())))
-        sp_phen = float(''.join(re.findall(r'\d+', fille.readline())))
-        sk_phen = float(''.join(re.findall(r'\d+', fille.readline())))
-        cal_phen = float(''.join(re.findall(r'\d+', fille.readline())))
-        fos_phen = float(''.join(re.findall(r'\d+', fille.readline())))
-        na_phen = float(''.join(re.findall(r'\d+', fille.readline())))
-        liz_phen = float(''.join(re.findall(r'\d+', fille.readline())))
-        met_phen = float(''.join(re.findall(r'\d+', fille.readline())))
-        fille.close()
+        c.execute("SELECT  kdg FROM komponents")
+        row = c.fetchmany(14)
+        kdg_phen = row[0][0]
+        kdg_yach = row[1][0]
+        kdg_kuk = row [2][0]
+        kdg_fish = row[3][0]
+        kdg_mk2 = row[4][0]
+        kdg_m_k = row[5][0]
+        kdg_yam = row[6][0]
+        kdg_gmih = row[7][0]
+        kdg_shr = row[8][0]
+        kdg_msrs = row[9][0]
+        kdg_trm = row[10][0]
+        kdg_r_k = row[11][0]
+        kdg_prem_8203 = row[12][0]
+        kdg_prem_8184 = row[13][0]
+        
+        c.execute("SELECT  sp FROM komponents")
+        row = c.fetchmany(14)
+        sp_phen =  row[0][0]
+        sp_yach = row[1][0]
+        sp_kuk = row [2][0]
+        sp_fish = row[3][0]
+        sp_mk2 = row[4][0]
+        sp_m_k = row[5][0]
+        sp_yam = row[6][0]
+        sp_gmih = row[7][0]
+        sp_shr = row[8][0]
+        sp_msrs = row[9][0]
+        sp_trm = row[10][0]
+        sp_r_k = row[11][0]
+        sp_prem_8203 = row[12][0]
+        sp_prem_8184 = row[13][0]
 
-        fille = open("./data/yach.ini", "r")
-        kdg_yach = float(''.join(re.findall(r'\d+', fille.readline())))
-        sp_yach = float(''.join(re.findall(r'\d+', fille.readline())))
-        sk_yach = float(''.join(re.findall(r'\d+', fille.readline())))
-        cal_yach = float(''.join(re.findall(r'\d+', fille.readline())))
-        fos_yach = float(''.join(re.findall(r'\d+', fille.readline())))
-        na_yach = float(''.join(re.findall(r'\d+', fille.readline())))
-        liz_yach = float(''.join(re.findall(r'\d+', fille.readline())))
-        met_yach = float(''.join(re.findall(r'\d+', fille.readline())))
-        fille.close()
+        c.execute("SELECT  sk FROM komponents")
+        row = c.fetchmany(14)
+        sk_phen =  row[0][0]
+        sk_yach = row[1][0]
+        sk_kuk = row [2][0]
+        sk_fish = row[3][0]
+        sk_mk2 = row[4][0]
+        sk_m_k = row[5][0]
+        sk_yam = row[6][0]
+        sk_gmih = row[7][0]
+        sk_shr = row[8][0]
+        sk_msrs = row[9][0]
+        sk_trm = row[10][0]
+        sk_r_k = row[11][0]
+        sk_prem_8203 = row[12][0]
+        sk_prem_8184 = row[13][0]
+        
+        c.execute("SELECT  cal FROM komponents")
+        row = c.fetchmany(14)
+        cal_phen =  row[0][0]
+        cal_yach = row[1][0]
+        cal_kuk = row [2][0]
+        cal_fish = row[3][0]
+        cal_mk2 = row[4][0]
+        cal_m_k = row[5][0]
+        cal_yam = row[6][0]
+        cal_gmih = row[7][0]
+        cal_shr = row[8][0]
+        cal_msrs = row[9][0]
+        cal_trm = row[10][0]
+        cal_r_k = row[11][0]
+        cal_prem_8203 = row[12][0]
+        cal_prem_8184 = row[13][0]
+        
+        c.execute("SELECT  fos FROM komponents")
+        row = c.fetchmany(14)
+        fos_phen =  row[0][0]
+        fos_yach = row[1][0]
+        fos_kuk = row [2][0]
+        fos_fish = row[3][0]
+        fos_mk2 = row[4][0]
+        fos_m_k = row[5][0]
+        fos_yam = row[6][0]
+        fos_gmih = row[7][0]
+        fos_shr = row[8][0]
+        fos_msrs = row[9][0]
+        fos_trm = row[10][0]
+        fos_r_k = row[11][0]
+        fos_prem_8203 = row[12][0]
+        fos_prem_8184 = row[13][0]
+        
+        c.execute("SELECT  na FROM komponents")
+        row = c.fetchmany(14)
+        na_phen =  row[0][0]
+        na_yach = row[1][0]
+        na_kuk = row [2][0]
+        na_fish = row[3][0]
+        na_mk2 = row[4][0]
+        na_m_k = row[5][0]
+        na_yam = row[6][0]
+        na_gmih = row[7][0]
+        na_shr = row[8][0]
+        na_msrs = row[9][0]
+        na_trm = row[10][0]
+        na_r_k = row[11][0]
+        na_prem_8203 = row[12][0]
+        na_prem_8184 = row[13][0]
 
-        fille = open("./data/kuk.ini", "r")
-        kdg_kuk = float(''.join(re.findall(r'\d+', fille.readline())))
-        sp_kuk = float(''.join(re.findall(r'\d+', fille.readline())))
-        sk_kuk = float(''.join(re.findall(r'\d+', fille.readline())))
-        cal_kuk = float(''.join(re.findall(r'\d+', fille.readline())))
-        fos_kuk = float(''.join(re.findall(r'\d+', fille.readline())))
-        na_kuk = float(''.join(re.findall(r'\d+', fille.readline())))
-        liz_kuk = float(''.join(re.findall(r'\d+', fille.readline())))
-        met_kuk = float(''.join(re.findall(r'\d+', fille.readline())))
-        fille.close()
+        c.execute("SELECT liz FROM komponents")
+        row = c.fetchmany(14)
+        liz_phen =  row[0][0]
+        liz_yach = row[1][0]
+        liz_kuk = row [2][0]
+        liz_fish = row[3][0]
+        liz_mk2 = row[4][0]
+        liz_m_k = row[5][0]
+        liz_yam = row[6][0]
+        liz_gmih = row[7][0]
+        liz_shr = row[8][0]
+        liz_msrs = row[9][0]
+        liz_trm = row[10][0]
+        liz_r_k = row[11][0]
+        liz_prem_8203 = row[12][0]
+        liz_prem_8184 = row[13][0]
+         
+        c.execute("SELECT met FROM komponents")
+        row = c.fetchmany(14)
+        met_phen =  row[0][0]
+        met_yach = row[1][0]
+        met_kuk = row [2][0]
+        met_fish = row[3][0]
+        met_mk2 = row[4][0]
+        met_m_k = row[5][0]
+        met_yam = row[6][0]
+        met_gmih = row[7][0]
+        met_shr = row[8][0]
+        met_msrs = row[9][0]
+        met_trm = row[10][0]
+        met_r_k = row[11][0]
+        met_prem_8203 = row[12][0]
+        met_prem_8184 = row[13][0]
 
-        fille = open("./data/mk.ini", "r")
-        kdg_m_k = float(''.join(re.findall(r'\d+', fille.readline())))
-        sp_m_k = float(''.join(re.findall(r'\d+', fille.readline())))
-        sk_m_k = float(''.join(re.findall(r'\d+', fille.readline())))
-        cal_m_k = float(''.join(re.findall(r'\d+', fille.readline())))
-        fos_m_k = float(''.join(re.findall(r'\d+', fille.readline())))
-        na_m_k = float(''.join(re.findall(r'\d+', fille.readline())))
-        liz_m_k = float(''.join(re.findall(r'\d+', fille.readline())))
-        met_m_k = float(''.join(re.findall(r'\d+', fille.readline())))
-        fille.close()
+        c.close()
+        conn.close()        
 
-        fille  = open("./data/fish.ini", "r")
-        kdg_fish = float(''.join(re.findall(r'\d+', fille.readline())))
-        sp_fish = float(''.join(re.findall(r'\d+', fille.readline())))
-        sk_fish = float(''.join(re.findall(r'\d+', fille.readline())))
-        cal_fish = float(''.join(re.findall(r'\d+', fille.readline())))
-        fos_fish = float(''.join(re.findall(r'\d+', fille.readline())))
-        na_fish = float(''.join(re.findall(r'\d+', fille.readline())))
-        liz_fish = float(''.join(re.findall(r'\d+', fille.readline())))
-        met_fish = float(''.join(re.findall(r'\d+', fille.readline())))
-        fille.close()
-
-        fille = open("./data/prem_8184.ini", "r")
-        kdg_prem_8184 = float(''.join(re.findall(r'\d+', fille.readline())))
-        sp_prem_8184 = float(''.join(re.findall(r'\d+', fille.readline())))
-        sk_prem_8184 = float(''.join(re.findall(r'\d+', fille.readline())))
-        cal_prem_8184 = float(''.join(re.findall(r'\d+', fille.readline())))
-        fos_prem_8184 = float(''.join(re.findall(r'\d+', fille.readline())))
-        na_prem_8184 = float(''.join(re.findall(r'\d+', fille.readline())))
-        liz_prem_8184 = float(''.join(re.findall(r'\d+', fille.readline())))
-        met_prem_8184 = float(''.join(re.findall(r'\d+', fille.readline())))
-        fille.close()
-
-        fille = open("./data/gmih.ini", "r")
-        kdg_gmih = float(''.join(re.findall(r'\d+', fille.readline())))
-        sp_gmih = float(''.join(re.findall(r'\d+', fille.readline())))
-        sk_gmih = float(''.join(re.findall(r'\d+', fille.readline())))
-        cal_gmih = float(''.join(re.findall(r'\d+', fille.readline())))
-        fos_gmih = float(''.join(re.findall(r'\d+', fille.readline())))
-        na_gmih = float(''.join(re.findall(r'\d+', fille.readline())))
-        liz_gmih = float(''.join(re.findall(r'\d+', fille.readline())))
-        met_gmih = float(''.join(re.findall(r'\d+', fille.readline())))
-        fille.close()
-
-        fille = open("./data/shr.ini", "r")
-        kdg_shr = float(''.join(re.findall(r'\d+', fille.readline())))
-        sp_shr = float(''.join(re.findall(r'\d+', fille.readline())))
-        sk_shr = float(''.join(re.findall(r'\d+', fille.readline())))
-        cal_shr = float(''.join(re.findall(r'\d+', fille.readline())))
-        fos_shr = float(''.join(re.findall(r'\d+', fille.readline())))
-        na_shr = float(''.join(re.findall(r'\d+', fille.readline())))
-        liz_shr = float(''.join(re.findall(r'\d+', fille.readline())))
-        met_shr = float(''.join(re.findall(r'\d+', fille.readline())))
-        fille.close()
-
-        fille = open("./data/mk2.ini", "r")
-        kdg_mk2 = float(''.join(re.findall(r'\d+', fille.readline())))
-        sp_mk2 = float(''.join(re.findall(r'\d+', fille.readline())))
-        sk_mk2 = float(''.join(re.findall(r'\d+', fille.readline())))
-        cal_mk2 = float(''.join(re.findall(r'\d+', fille.readline())))
-        fos_mk2 = float(''.join(re.findall(r'\d+', fille.readline())))
-        na_mk2 = float(''.join(re.findall(r'\d+', fille.readline())))
-        liz_mk2 = float(''.join(re.findall(r'\d+', fille.readline())))
-        met_mk2 = float(''.join(re.findall(r'\d+', fille.readline())))
-        fille.close()
-
-        fille = open("./data/yam.ini", "r")
-        kdg_yam = float(''.join(re.findall(r'\d+', fille.readline())))
-        sp_yam = float(''.join(re.findall(r'\d+', fille.readline())))
-        sk_yam = float(''.join(re.findall(r'\d+', fille.readline())))
-        cal_yam = float(''.join(re.findall(r'\d+', fille.readline())))
-        fos_yam = float(''.join(re.findall(r'\d+', fille.readline())))
-        na_yam = float(''.join(re.findall(r'\d+', fille.readline())))
-        liz_yam = float(''.join(re.findall(r'\d+', fille.readline())))
-        met_yam = float(''.join(re.findall(r'\d+', fille.readline())))
-        fille.close()
-
-        fille = open("./data/trm.ini", "r")
-        kdg_trm = float(''.join(re.findall(r'\d+', fille.readline())))
-        sp_trm = float(''.join(re.findall(r'\d+', fille.readline())))
-        sk_trm = float(''.join(re.findall(r'\d+', fille.readline())))
-        cal_trm = float(''.join(re.findall(r'\d+', fille.readline())))
-        fos_trm = float(''.join(re.findall(r'\d+', fille.readline())))
-        na_trm = float(''.join(re.findall(r'\d+', fille.readline())))
-        liz_trm = float(''.join(re.findall(r'\d+', fille.readline())))
-        met_trm = float(''.join(re.findall(r'\d+', fille.readline())))
-        fille.close()
-
-        fille = open("./data/msrs.ini", "r")
-        kdg_msrs = float(''.join(re.findall(r'\d+', fille.readline())))
-        sp_msrs = float(''.join(re.findall(r'\d+', fille.readline())))
-        sk_msrs = float(''.join(re.findall(r'\d+', fille.readline())))
-        cal_msrs = float(''.join(re.findall(r'\d+', fille.readline())))
-        fos_msrs = float(''.join(re.findall(r'\d+', fille.readline())))
-        na_msrs = float(''.join(re.findall(r'\d+', fille.readline())))
-        liz_msrs = float(''.join(re.findall(r'\d+', fille.readline())))
-        met_msrs = float(''.join(re.findall(r'\d+', fille.readline())))
-        fille.close()
-
-        fille = open("./data/rk.ini", "r")
-        kdg_r_k = float(''.join(re.findall(r'\d+', fille.readline())))
-        sp_r_k = float(''.join(re.findall(r'\d+', fille.readline())))
-        sk_r_k = float(''.join(re.findall(r'\d+', fille.readline())))
-        cal_r_k = float(''.join(re.findall(r'\d+', fille.readline())))
-        fos_r_k = float(''.join(re.findall(r'\d+', fille.readline())))
-        na_r_k = float(''.join(re.findall(r'\d+', fille.readline())))
-        liz_r_k = float(''.join(re.findall(r'\d+', fille.readline())))
-        met_r_k = float(''.join(re.findall(r'\d+', fille.readline())))
-        fille.close()
-
-        fille = open("./data/prem_8203.ini", "r")
-        kdg_prem_8203 = float(''.join(re.findall(r'\d+', fille.readline())))
-        sp_prem_8203 = float(''.join(re.findall(r'\d+', fille.readline())))
-        sk_prem_8203 = float(''.join(re.findall(r'\d+', fille.readline())))
-        cal_prem_8203 = float(''.join(re.findall(r'\d+', fille.readline())))
-        fos_prem_8203 = float(''.join(re.findall(r'\d+', fille.readline())))
-        na_prem_8203 = float(''.join(re.findall(r'\d+', fille.readline())))
-        liz_prem_8203 = float(''.join(re.findall(r'\d+', fille.readline())))
-        met_prem_8203 = float(''.join(re.findall(r'\d+', fille.readline())))
-        fille.close()
-
-        kdg = ((kdg_phen/100) * phen) + ((kdg_yach/100) * yach) + ((kdg_kuk/100) * kuk) + ((kdg_m_k/100) * m_k) + ((kdg_fish/100) * fish) + ((kdg_prem_8184/100) * prem_8184) + ((kdg_prem_8203/100) * prem_8203)  + ((kdg_gmih/100) * gmih) + ((kdg_shr/100) * shr) + ((kdg_mk2/100) * mk2) + ((kdg_yam/100) * yam) + ((kdg_trm/100) * trm) + ((kdg_msrs/100) * msrs) + ((kdg_r_k/100) * r_k)
-        sp = ((sp_phen/1000) * phen) + ((sp_yach/1000) * yach) + ((sp_kuk/100) * kuk) + ((sp_m_k/1000) * m_k) + ((sp_fish/1000) * fish) + ((sp_prem_8184/10000) * prem_8184) + ((sp_prem_8203/100) * prem_8203)  + ((sp_gmih/1000) * gmih) + ((sp_shr/100) * shr) + ((sp_mk2/100) * mk2) + ((sp_yam/100) * yam) + ((sp_trm/1000) * trm) + ((sp_msrs/1000) * msrs) + ((sp_r_k/100) * r_k)
-        sk = ((sk_phen/1000) * phen) + ((sk_yach/1000) * yach) + ((sk_kuk/1000) * kuk) + ((sk_m_k/100) * m_k) + ((sk_fish/100) * fish) + ((sk_prem_8184/100) * prem_8184) + ((sk_prem_8203/100) * prem_8203) + ((sk_gmih/100) * gmih) + ((sk_shr/100) * shr) + ((sk_mk2/100) * mk2) + ((sk_yam/100) * yam) + ((sk_trm/100) * trm) + ((sk_msrs/100) * msrs) + ((sk_r_k/100) * r_k)
-        cal =((cal_phen/10000) * phen) + ((cal_yach/10000) * yach) + ((cal_kuk/10000) * kuk) + ((cal_m_k/1000) * m_k) + ((cal_fish/1000) * fish) + ((cal_prem_8184/100) * prem_8184) + ((cal_prem_8203/10000) * prem_8203)  + ((cal_gmih/10000) * gmih) + ((cal_shr/10000) * shr) + ((cal_mk2/10000) * mk2) + ((cal_yam/10000) * yam) + ((cal_trm/10000) * trm) + ((cal_msrs/100) * msrs) + ((cal_r_k/100) * r_k) 
-        fos = ((fos_phen/1000) * phen) + ((fos_yach/10000) * yach) + ((fos_kuk/1000) * kuk) + ((fos_m_k/10000) * m_k) + ((fos_fish/1000) * fish) + ((fos_prem_8184/10000) * prem_8184) + ((fos_prem_8203/10000) * prem_8203) + ((fos_gmih/10000) * gmih) + ((fos_shr/10000) * shr) + ((fos_mk2/10000) * mk2) + ((fos_yam/10000) * yam) + ((fos_trm/10000) * trm) + ((fos_msrs/100) * msrs) + ((fos_r_k/100) * r_k)
-        na = ((na_phen/10000) * phen) + ((na_yach/10000) * yach) + ((na_kuk/10000) * kuk) + ((na_m_k/10000) * m_k) + ((na_fish/10000) * fish) + ((na_prem_8184/100) * prem_8184) + ((na_prem_8203/1000) * prem_8203)  + ((na_gmih/10000) * gmih) + ((na_shr/10000) * shr) + ((na_mk2/10000) * mk2) + ((na_yam/100) * yam) + ((na_trm/10000) * trm) + ((na_msrs/100) * msrs) + ((na_r_k/100) * r_k)
-        liz = ((liz_phen/10000) * phen) + ((liz_yach/1000) * yach) + ((liz_kuk/10000) * kuk) + ((liz_m_k/10000) * m_k) + ((liz_fish/10000) * fish) + ((liz_prem_8184/10000) * prem_8184) + ((liz_prem_8203/10000) * prem_8203)  + ((liz_gmih/10000) * gmih) + ((liz_shr/10000) * shr) + ((liz_mk2/10000) * mk2) + ((liz_yam/10000) * yam) + ((liz_trm/10000) * trm) + ((liz_msrs/100) * msrs) + ((liz_r_k/100) * r_k)
-        met = ((met_phen/10000) * phen) + ((met_yach/10000) * yach) + ((met_kuk/10000) * kuk) + ((met_m_k/1000) * m_k) + ((met_fish/10000) * fish) + ((met_prem_8184/10000) * prem_8184) + ((met_prem_8203/10000) * prem_8203) + ((met_gmih/10000) * gmih) + ((met_shr/1000) * shr) + ((met_mk2/10000) * mk2) + ((met_yam/10000) * yam) + ((met_trm/10000) * trm) + ((met_msrs/100) * msrs) + ((met_r_k/100) * r_k)
+        kdg = ((kdg_phen*phen)+(kdg_yach* yach) + (kdg_kuk* kuk) + (kdg_m_k* m_k) + (kdg_fish* fish) + (kdg_prem_8184* prem_8184) + (kdg_prem_8203* prem_8203)  + (kdg_gmih* gmih) + (kdg_shr* shr) + (kdg_mk2* mk2) + (kdg_yam* yam) + (kdg_trm* trm) + (kdg_msrs* msrs) + (kdg_r_k* r_k))/100
+        sp = ((sp_phen* phen) + (sp_yach* yach) + (sp_kuk* kuk) + (sp_m_k* m_k) + (sp_fish* fish) + (sp_prem_8184* prem_8184) + (sp_prem_8203* prem_8203)  + (sp_gmih* gmih) + (sp_shr* shr) + (sp_mk2* mk2) + (sp_yam* yam) + (sp_trm* trm) + (sp_msrs* msrs) + (sp_r_k* r_k))/100
+        sk = ((sk_phen* phen) + (sk_yach* yach) + (sk_kuk* kuk) + (sk_m_k* m_k) + (sk_fish* fish) + (sk_prem_8184* prem_8184) + (sk_prem_8203* prem_8203) + (sk_gmih* gmih) + (sk_shr* shr) + (sk_mk2* mk2) + (sk_yam* yam) + (sk_trm* trm) + (sk_msrs * msrs) + (sk_r_k* r_k))/100
+        cal =((cal_phen* phen) + (cal_yach* yach) + (cal_kuk* kuk) + (cal_m_k* m_k) + (cal_fish* fish) + (cal_prem_8184* prem_8184) + (cal_prem_8203* prem_8203)  + (cal_gmih* gmih) + (cal_shr* shr) + (cal_mk2* mk2) + (cal_yam* yam) + (cal_trm* trm) + (cal_msrs* msrs) + (cal_r_k* r_k))/100 
+        fos = ((fos_phen* phen) + (fos_yach* yach) + (fos_kuk* kuk) + (fos_m_k* m_k) + (fos_fish* fish) + (fos_prem_8184* prem_8184) + (fos_prem_8203* prem_8203) + (fos_gmih* gmih) + (fos_shr* shr) + (fos_mk2* mk2) + (fos_yam* yam) + (fos_trm* trm) + (fos_msrs* msrs) + (fos_r_k* r_k))/100
+        na = ((na_phen* phen) + (na_yach* yach) + (na_kuk* kuk) + (na_m_k* m_k) + (na_fish* fish) + (na_prem_8184* prem_8184) + (na_prem_8203* prem_8203)  + (na_gmih* gmih) + (na_shr* shr) + (na_mk2* mk2) + (na_yam* yam) + (na_trm* trm) + (na_msrs* msrs) + (na_r_k* r_k))/100
+        liz = ((liz_phen* phen) + (liz_yach* yach) + (liz_kuk* kuk) + (liz_m_k* m_k) + (liz_fish* fish) + (liz_prem_8184* prem_8184) + (liz_prem_8203* prem_8203)  + (liz_gmih* gmih) + (liz_shr* shr) + (liz_mk2* mk2) + (liz_yam* yam) + (liz_trm* trm) + (liz_msrs* msrs) + (liz_r_k* r_k))/100
+        met = ((met_phen* phen) + (met_yach* yach) + (met_kuk* kuk) + (met_m_k* m_k) + (met_fish* fish) + (met_prem_8184* prem_8184) + (met_prem_8203* prem_8203) + (met_gmih* gmih) + (met_shr* shr) + (met_mk2* mk2) + (met_yam* yam) + (met_trm* trm) + (met_msrs* msrs) + (met_r_k* r_k))/100
 
         self.calc_end(kdg, sp, sk, cal, fos, na, liz, met)
         
